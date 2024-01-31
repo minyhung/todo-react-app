1c77a9db6db66725bc8bdb5d62a8eba7cfb96cd4
minbro <mhlee0910@naver.com>
2024-01-31 12:07:45 +0900

    'commit'

--- src/App.js
+++ src/App.js
@@ -16,11 +16,32 @@ class App extends React.Component {
 
     this.state = {
       items: [
-        { id: 0, "title": "React", done: true },
-        { id: 1, "title": "vue", done: false },
-        { id: 2, "title": "angular", done: false }
-      ]
-    };
+        
+      ]}
+  }
+
+  //화면이 보여질 때 마다 호출되는 수명주기 함수
+  componentDidMount(){
+    //ajax 요청 객체 생성
+    let request = new XMLHttpRequest();
+
+    //요청준비
+    request.open('GET',"http://127.0.0.1/todo?userid=hyung");
+
+    //요청
+    request.send('');
+    //응답처리
+    request.addEventListener('load' , () => {
+      //json 데이터 출력
+      //console.log(request.responseText)
+
+
+      let data = JSON.parse(request.responseText);
+      console.log(data)
+
+      //서버에서 받아온 데이터를 state에 대입해서 화면에 출력
+      this.setState({items:data.list})
+    })
   }
 
   add=(item)=> {
@@ -42,17 +63,33 @@ class App extends React.Component {
     //복사한 데이터를 다시 state에 적용
     this.setState({items:thisItems});
   }
+  //삭제를 위한 메서드
+  delete = (item) => {
+    //state 나 props의 데이터는 직접 편집이 안됩니다.
+    const thisItems = this.state.items;
+
+    //복사본에서 item을 제거
+    //filter 함수는 리턴 타입이 boolean 함수를 매개변수로 받아서
+    //리턴 결과가 true인 데이터만 모아서 배열로 리턴하는 함수입니다.
+
+    const newItems = thisItems.filter((e)=> e.id !== item.id);
+    
+    //원본에 다시복사
+    this.setState({items:newItems})
+  }
 
   render() {
+
     // 배열을 순회하면서 출력할 내용을 생성
     // item은 배열을 순회할 때 각각의 데이터이고 idx는 인덱스
     // 배열을 순회하면서 출력물을 만들 때는 key를 설정해주어야 합니다.
     // key를 설정하지 않으면 출력에는 문제가 없지만 콘솔에 에러가 출력됩니다.
+    
     var display = this.state.items.length>0 && (
       <Paper style={{margin:16}}>
         <List>
           {this.state.items.map((item,idx)=>(
-            <ToDo item ={item} id={idx}/>
+            <ToDo item ={item} id={idx} delete = {this.delete} />
           ))}
         </List>
       </Paper>
@@ -68,7 +105,6 @@ class App extends React.Component {
         
       </Container>
     </div>
-     
     )
   }
 }
--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -4,8 +4,13 @@ import {
 		ListItem,
 		ListItemText,
 		InputBase,
-		Checkbox
+		Checkbox,
+		ListItemSecondaryAction,
+		IconButton
 } from "@material-ui/core"
+
+
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined"
 
 import {Paper, List} from "@material-ui/core"
 
@@ -15,7 +20,8 @@ class ToDo extends React.Component{
 	constructor(props){
 		//App.js에서 넘겨준 속성들은 자신의 props에 저장
 		//this.props.item이라는 속성에 앞에서 전달한 데이터를 저장
-	
+
+
 		super(props);  // 상위 클래스에서 넘겨준 모든 props를 현재 클래스에 저장
 		//state 생성 
 		//item이라는 이름으로 props 중에서 item이라는 값을 state로 저장
@@ -24,8 +30,14 @@ class ToDo extends React.Component{
 		//state로 변환해야합니다.
 
 		this.state = {item:this.props.item}
-	}
 
+		this.delete = this.props.delete
+
+	}
+	//삭제 버튼을 누를 때 호출될 이벤트 핸들러
+	deleteEventHandler = (e) =>{
+		this.delete(this.state.item)
+	}
 
 	//화면에 출력할 내용을 리턴하는 메서드
 
@@ -46,6 +58,11 @@ class ToDo extends React.Component{
 						fullwidth={true}
 					/>
 				</ListItemText>
+				<ListItemSecondaryAction>
+					<IconButton aria-label="Delete ToDo" onClick={this.deleteEventHandler}>
+						<DeleteOutlined />
+					</IconButton>
+				</ListItemSecondaryAction>
 			</ListItem>
 		)
 	}
